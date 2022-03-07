678ee3db3334ad11224bed585dba4afa16d63115
ny0011 <nayun011@gmail.com>
2022-03-07 15:31:43 +0900

    5.8 Form Errors

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Performant, flexible and extensible forms with easy-to-use validation.
 
 - https://react-hook-form.com/
 - 쉽게 유효성을 확인하는 라이브러리
-- useForm()에서 사용할 함수
+- useForm()에서 사용할 함수와 변수
   - register : onChange 함수를 대체 가능
   - handleSubmit : onSubmit 함수를 대체 가능
+  - formState : error가 발생한 input의 값과 상태, 메시지를 확인할 수 있는 변수
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,18 +1,44 @@
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+}
+
 function ToDoList() {
-  const { register, handleSubmit, formState } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
   const onValid = (data: any) => {
     console.log(data);
   };
-  console.log(formState.errors);
   return (
     <div>
       <form
         style={{ display: "flex", flexDirection: "column" }}
         onSubmit={handleSubmit(onValid)}
       >
-        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com Emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
         <input
           {...register("firstName", { required: true })}
           placeholder="First Name"
