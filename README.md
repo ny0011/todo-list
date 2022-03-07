9d712d104b68cc38e0c0116c5033789fb0773bd5
ny0011 <nayun011@gmail.com>
2022-03-07 16:34:50 +0900

    5.11 Add To Do

--- README.md
+++ README.md
@@ -15,3 +15,13 @@ Performant, flexible and extensible forms with easy-to-use validation.
   - handleSubmit : onSubmit 함수를 대체 가능
   - formState : error가 발생한 input의 값과 상태, 메시지를 확인할 수 있는 변수
   - setError : error를 발생시키고 싶을 때 사용
+  - setValue : input 값을 변경하고 싶을 때 사용
+
+## Recoil with typescript
+
+- 아래처럼 생성할 atom을 등록할 때 typescript에서는 toDos의 타입이 never로 나옴
+  - toDos의 타입을 위한 interface를 새로 생성해주기
+
+```
+const [toDos, setToDos] = useRecoilState(toDoState);
+```
---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,52 @@
+import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
+
+interface IForm {
+  toDo: string;
+}
+
+interface IToDo {
+  text: string;
+  category: "TO_DO" | "DOING" | "DONE";
+  id: number;
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
+function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, category: "TO_DO", id: Date.now() },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", {
+            required: "Please write a To Do",
+          })}
+          placeholder="Write a To Do"
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
