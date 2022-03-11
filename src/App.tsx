0ae9326fb29d43d07885b322919a12695e9d2731
ny0011 <nayun011@gmail.com>
2022-03-11 19:30:42 +0900

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  DropResult,
+  DraggableLocation,
+} from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
@@ -23,47 +27,61 @@ const Boards = styled.div`
   gap: 10px;
 `;
 
+interface IInfo {
+  destination: DraggableLocation;
+  source: DraggableLocation;
+}
+
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
+  const deleteToDos = (source: DraggableLocation) => {
+    setToDos((allBoards) => {
+      const board = [...allBoards[source.droppableId]];
+      board.splice(source.index, 1);
+      return { ...allBoards, [source.droppableId]: board };
+    });
+  };
+  const changeToDosWithSameBoard = ({ destination, source }: IInfo) => {
+    setToDos((allBoards) => {
+      const board = [...allBoards[source.droppableId]];
+      const card = board[source.index];
+      board.splice(source.index, 1);
+      board.splice(destination.index, 0, card);
+      return {
+        ...allBoards,
+        [source.droppableId]: board,
+      };
+    });
+  };
+  const changeToDosWithDiffrentBoard = ({ destination, source }: IInfo) => {
+    setToDos((allBoards) => {
+      const sourceBoard = [...allBoards[source.droppableId]];
+      const sourceCard = sourceBoard[source.index];
+      sourceBoard.splice(source.index, 1);
+      const destinationBoard = [...allBoards[destination.droppableId]];
+      destinationBoard.splice(destination.index, 0, sourceCard);
+      return {
+        ...allBoards,
+        [source.droppableId]: sourceBoard,
+        [destination.droppableId]: destinationBoard,
+      };
+    });
+  };
+
   const onDragEnd = (info: DropResult) => {
     const { destination, source } = info;
 
     if (!destination) return;
     if (destination.droppableId === "trashcan") {
-      setToDos((allBoards) => {
-        const board = [...allBoards[source.droppableId]];
-        board.splice(source.index, 1);
-        return { ...allBoards, [source.droppableId]: board };
-      });
-      return;
+      return deleteToDos(source);
     }
     if (destination.droppableId === source.droppableId) {
       if (destination.index === source.index) return;
-      setToDos((allBoards) => {
-        const board = [...allBoards[source.droppableId]];
-        const card = board[source.index];
-        board.splice(source.index, 1);
-        board.splice(destination.index, 0, card);
-        return {
-          ...allBoards,
-          [source.droppableId]: board,
-        };
-      });
-    } else {
-      setToDos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const sourceCard = sourceBoard[source.index];
-        sourceBoard.splice(source.index, 1);
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        destinationBoard.splice(destination.index, 0, sourceCard);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
+      return changeToDosWithSameBoard({ destination, source });
     }
+    return changeToDosWithDiffrentBoard({ destination, source });
   };
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
